b52de8f6bab141fdfc7cc385b9d0e2783a456f2d
Friedemann Zenke <friedemann.zenke@epfl.ch>
2015-01-28 20:28:17 +0100

    Refines save/load in SpikeDelay

--- src/SpikeDelay.h
+++ src/SpikeDelay.h
@@ -50,46 +50,30 @@ class SpikeDelay
 		friend class boost::serialization::access;
 		template<class Archive>
 		void save(Archive & ar, const unsigned int version) const
-			{
-				for (NeuronID i = 0 ; i < ndelay ; ++i ) {
-					SpikeContainer * sc = delaybuf[i];
-					NeuronID s = (int) (sc->size());
-					ar & s;
-					for (SpikeContainer::const_iterator spike = sc->begin() ; 
-							spike != sc->end() ; ++spike ) {
-							ar & *spike;
-					}
-
-					AttributeContainer * ac = attribbuf[i];
-					s = (int) (ac->size());
-					ar & s;
-					for (AttributeContainer::const_iterator attrib = ac->begin() ; 
-							attrib != ac->end() ; ++attrib ) {
-							ar & *attrib;
-					}
-				}
+		{
+			for (NeuronID i = 0 ; i < ndelay ; ++i ) {
+				SpikeContainer * sc = delaybuf[i];
+				ar & *sc;
 			}
+
+			for (NeuronID i = 0 ; i < ndelay ; ++i ) {
+				AttributeContainer * ac = attribbuf[i];
+				ar & *ac;
+			}
+		}
 		template<class Archive>
 		void load(Archive & ar, const unsigned int version)
-			{
-				for (NeuronID i = 0 ; i < ndelay ; ++i ) {
-					NeuronID len;
-					ar & len;
-					delaybuf[i]->clear();
-					for (NeuronID j = 0 ; j < len ; ++j) {
-						NeuronID spike;
-						ar & spike;
-						delaybuf[i]->push_back(spike);
-					}
-					ar & len;
-					attribbuf[i]->clear();
-					for (NeuronID j = 0 ; j < len ; ++j) {
-						AurynFloat attrib;
-						ar & attrib;
-						attribbuf[i]->push_back(attrib);
-					}
-				}
+		{
+			for (NeuronID i = 0 ; i < ndelay ; ++i ) {
+				SpikeContainer * sc = delaybuf[i];
+				ar & *sc;
 			}
+
+			for (NeuronID i = 0 ; i < ndelay ; ++i ) {
+				AttributeContainer * ac = attribbuf[i];
+				ar & *ac;
+			}
+		}
 		BOOST_SERIALIZATION_SPLIT_MEMBER()
 
 		SpikeContainer ** delaybuf;
@@ -133,8 +117,5 @@ class SpikeDelay
 		void clear();
 };
 
-BOOST_CLASS_TRACKING(SpikeDelay,track_never)
-// BOOST_CLASS_IMPLEMENTATION(SpikeDelay,object_serializable)
-// BOOST_SERIALIZATION_ASSUME_ABSTRACT(SpikeDelay)
 
 #endif /*SPIKEDELAY_H_*/
