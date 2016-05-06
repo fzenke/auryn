d47e3e7f772e9a79f45878fd11ff1d65465d7e9d
Friedemann Zenke <fzenke@stanford.edu>
2016-05-06 10:09:52 +0200

    Fixes test routines

--- tests/coba_test/run_coba_parallel_test.sh
+++ tests/coba_test/run_coba_parallel_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUILDDIR="../../build/release"
+TOOLDIR="$BUILDDIR/tools"
 
 # Compile code
 make -C $BUILDDIR
@@ -18,7 +19,7 @@ WMATLOADSTRING="--fee pynn.ee.wmat --fei pynn.ei.wmat --fie pynn.ie.wmat --fii p
 FILTERSPIKES="awk '{ if ($2%2==0) print }'" 
 mpirun -n 2 $BUILDDIR/examples/sim_coba_binmon $WMATLOADSTRING --simtime $SIMTIME --dir $TMPDIR 
 $CMD_BENCHMARK1
-aube -i $TMPDIR/coba.*.e.spk | awk '{ if ($2%2==0) print }' | tee multicpu.ras | md5sum | cut -d " " -f 1 > coba_checksums.txt
+$TOOLDIR/aube -i $TMPDIR/coba.*.e.spk | awk '{ if ($2%2==0) print }' | tee multicpu.ras | md5sum | cut -d " " -f 1 > coba_checksums.txt
 rm -r $TMPDIR
 
 
