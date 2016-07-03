6dd1657caa060a5eb231dcfb6a4297d944782031
Friedemann Zenke <fzenke@stanford.edu>
2016-07-02 20:50:54 -0700

    Commits changes

--- src/auryn_version_info.cpp
+++ src/auryn_version_info.cpp
@@ -1,3 +1,25 @@
-// This file was intentionally left empty and exists only for the sake of Makefile wildcards
 #include "auryn_version_info.h"
+#include "auryn_definitions.h"
+
+namespace auryn {
+
+/*! The current Auryn version/revision number. */
+static const int auryn_version=0;
+static const int auryn_subversion=8;
+static const int auryn_revision_number=0;
+
+/*! The current Auryn revision suffix. */
+const char auryn_revision_suffix[] = "-dev-eab7157";
+
+/*! The current Auryn revision string from git describe */
+const char auryn_git_describe[] = "v0.8.0-dev-1-geab7157";
+
+/*! \brief Tag for header in binary encoded spike monitor files. 
+ *
+ * The first digits are 28796 for Auryn in 
+ * phone dial notation. The remaining 4 digits encode type of binary file and the current Auryn 
+ * version */
+static const NeuronID tag_binary_spike_monitor = 287960000+100*auryn_version+10*auryn_subversion+1*auryn_revision_number;
+static const NeuronID tag_binary_state_monitor = 287961000+100*auryn_version+10*auryn_subversion+1*auryn_revision_number;
+}
 
--- src/auryn_version_info.h
+++ src/auryn_version_info.h
@@ -1,27 +1,18 @@
 #define AURYN_VERSION_INFO_H_
 #ifndef AURYN_VERSION_INFO_H_
 
+#include <string>
+#include "auryn_definitions.h"
+
 namespace auryn {
 
-/*! The current Auryn version/revision number. 
- *  Should be all ints. */
-const int auryn_version=0;
-const int auryn_subversion=8;
-const int auryn_revision_number=0;
-
-/*! The current Auryn revision suffix. */
-const char auryn_revision_suffix[] = "-dev-57cd2fb";
-
-/*! The current Auryn revision string from git describe */
-const char auryn_git_describe[] = "v0.8.0-dev-1-g57cd2fb";
-
-/*! \brief Tag for header in binary encoded spike monitor files. 
- *
- * The first digits are 28796 for Auryn in 
- * phone dial notation. The remaining 4 digits encode type of binary file and the current Auryn 
- * version */
-const NeuronID tag_binary_spike_monitor = 287960000+100*auryn_version+10*auryn_subversion+1*auryn_revision_number;
-const NeuronID tag_binary_state_monitor = 287961000+100*auryn_version+10*auryn_subversion+1*auryn_revision_number;
+extern static const int auryn_version;
+extern static const int auryn_subversion;
+extern static const int auryn_revision_number;
+extern const char auryn_revision_suffix[];
+extern const char auryn_git_describe[];
+extern static const NeuronID tag_binary_spike_monitor;
+extern static const NeuronID tag_binary_state_monitor;
 
 }
 
