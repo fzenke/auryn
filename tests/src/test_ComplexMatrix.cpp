ce361d6573d5ac2044e78c6d21959064dbd8dd38
Friedemann Zenke <fzenke@stanford.edu>
2016-08-02 17:54:39 -0700

    Updates unite test suite for ComplexMatrix

--- tests/src/test_ComplexMatrix.cpp
+++ tests/src/test_ComplexMatrix.cpp
@@ -35,8 +35,8 @@ BOOST_AUTO_TEST_CASE( dense_fill_and_read ) {
 
 BOOST_AUTO_TEST_CASE( sparse_fill_and_read ) {
 
-	int nb_pre = 12;
-	int nb_post = 17;
+	int nb_pre = 52;
+	int nb_post = 21;
 	auryn::ComplexMatrix<int> mat(nb_pre,nb_post,nb_pre*nb_post);
 
 	int count = 0;
@@ -52,51 +52,87 @@ BOOST_AUTO_TEST_CASE( sparse_fill_and_read ) {
 	mat.fill_zeros();
 	BOOST_CHECK_EQUAL( mat.get_nonzero(), count );
 
-	for (int j = 0 ; j<nb_pre ; j += 1)
-	{
+	for (int j = 0 ; j<nb_pre ; j += 1) {
 		for (int i = 0 ; i<nb_post ; i += 1) {
 			int el = mat.get(j,i);
+			// std::cout << j << " " << i << " sh:" << element(j,i) << " is:" << el << std::endl;
 			if ( element(j,i)%2 ) {
 				BOOST_CHECK_EQUAL( el, element(j,i) );
 			} else {
-				std::cout << j << " " << i << " sh:" << element(j,i) << " is:" << el << std::endl;
 				BOOST_CHECK_EQUAL( el, 0 );
 			}
 		}
 	}
 }
 
-// BOOST_AUTO_TEST_CASE( sparse_complex_fill_and_read ) {
-// 
-// 	int nb_pre = 65;
-// 	int nb_post = 17;
-// 	auryn::ComplexMatrix<int> mat(nb_pre,nb_post,nb_pre*nb_post);
-// 	mat.set_num_synaptic_states(3);
-// 
-// 	// sparse fill
-// 	for (int j = 0 ; j<nb_pre ; j += 1)
-// 	{
-// 		for (int i = 0 ; i<nb_post; i += 1) {
-// 			if ( element(j,i)%2 ) {
-// 				mat.push_back(j,i,element(j,i));
-// 			}
-// 		}
-// 	}
-// 	mat.fill_zeros();
-// 
-// 	auryn::AurynVector<int, auryn::AurynLong> * sv = mat.get_synaptic_state_vector(2);
-// 	sv->set_random_normal(0.0,20);
-// 	auryn::AurynVector<int, auryn::AurynLong> * cp = new auryn::AurynVector<int, auryn::AurynLong>(mat.get_synaptic_state_vector(2));
-// 	mat.get_synaptic_state_vector(1)->diff(mat.get_synaptic_state_vector(0),mat.get_synaptic_state_vector(2));
-// 
-// 	for (int j = 0 ; j<nb_pre ; j += 1)
-// 	{
-// 		for (int i = 0 ; i<nb_pre ; i += 1) {
-// 			int el = mat.get(j,i);
-// 			if ( el%2 ) {
-// 				BOOST_CHECK_EQUAL( el, element(j,i) );
-// 				// BOOST_CHECK_EQUAL( mat.get(i,j), element(j,i) );
-// 			}
-// 		}
-// 	}
-// }
+BOOST_AUTO_TEST_CASE( sparse_complex_fill_and_read ) {
+	int nb_pre = 65;
+	int nb_post = 17;
+	auryn::ComplexMatrix<int> mat(nb_pre,nb_post,nb_pre*nb_post);
+	mat.set_num_synaptic_states(3);
+
+	// sparse fill
+	for (int j = 0 ; j<nb_pre ; j += 1)
+	{
+		for (int i = 0 ; i<nb_post; i += 1) {
+			if ( element(j,i)%2 ) {
+				mat.push_back(j,i,element(j,i));
+			}
+		}
+	}
+	mat.fill_zeros();
+
+	mat.get_synaptic_state_vector(1)->copy(mat.get_synaptic_state_vector(0));
+	mat.get_synaptic_state_vector(1)->scale(2);
+	mat.get_synaptic_state_vector(2)->copy(mat.get_synaptic_state_vector(1));
+	mat.get_synaptic_state_vector(2)->mul(mat.get_synaptic_state_vector(0));
+
+	for (int j = 0 ; j<nb_pre ; j += 1)
+	{
+		for (int i = 0 ; i<nb_post ; i += 1) {
+			int tg = element(j,i);
+			if ( tg%2 ) {
+				BOOST_CHECK_EQUAL( mat.get(j,i,0), tg );
+				BOOST_CHECK_EQUAL( mat.get(j,i,1), 2*tg );
+				BOOST_CHECK_EQUAL( mat.get(j,i,2), 2*tg*tg );
+			}
+		}
+	}
+}
+
+BOOST_AUTO_TEST_CASE( sparse_complex_prune ) {
+	int nb_pre = 65;
+	int nb_post = 17;
+	auryn::ComplexMatrix<int> mat(nb_pre,nb_post,nb_pre*nb_post);
+	mat.set_num_synaptic_states(3);
+
+	// sparse fill
+	for (int j = 0 ; j<nb_pre ; j += 1)
+	{
+		for (int i = 0 ; i<nb_post; i += 1) {
+			if ( element(j,i)%2 ) {
+				mat.push_back(j,i,element(j,i));
+			}
+		}
+	}
+	mat.fill_zeros();
+
+	mat.prune();
+
+	mat.get_synaptic_state_vector(1)->copy(mat.get_synaptic_state_vector(0));
+	mat.get_synaptic_state_vector(1)->scale(2);
+	mat.get_synaptic_state_vector(2)->copy(mat.get_synaptic_state_vector(1));
+	mat.get_synaptic_state_vector(2)->mul(mat.get_synaptic_state_vector(0));
+
+	for (int j = 0 ; j<nb_pre ; j += 1)
+	{
+		for (int i = 0 ; i<nb_post ; i += 1) {
+			int tg = element(j,i);
+			if ( tg%2 ) {
+				BOOST_CHECK_EQUAL( mat.get(j,i,0), tg );
+				BOOST_CHECK_EQUAL( mat.get(j,i,1), 2*tg );
+				BOOST_CHECK_EQUAL( mat.get(j,i,2), 2*tg*tg );
+			}
+		}
+	}
+}
