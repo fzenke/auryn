741eec66a45d12aec7005fd4365c8986a9155923
Friedemann Zenke <fzenke@stanford.edu>
2016-08-09 20:12:42 -0700

    Some small changes

--- tools/experimental/loadspk.py
+++ tools/experimental/loadspk.py
@@ -5,9 +5,14 @@ import struct
 
 
 class AurynBinarySpikeFile:
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, filename, debug_output=False):
+        # These params might have to adapted ot different Auryn datatypes and versions
         self.data_format = "@II"
+        self.class_version = (0,8,0)
+
+        self.debug = debug_output
+
+        self.filename = filename
         self.frame_size = struct.calcsize(self.data_format)
         self.datafile = open(filename, "rb")
 
@@ -17,16 +22,22 @@ class AurynBinarySpikeFile:
         self.timestep = 1.0/self.header[0]
         version_code = self.header[1]%1000
         self.file_version = (version_code%10, (version_code%100)/10, (version_code%1000)/100)
-        print "File version", self.file_version
-        print "File data struct size", self.frame_size
+        if self.class_version != self.file_version:
+            print("Warning! Version mismatch between the decoding tool and the file version.")
+            print("AurynBinarySpikeFile %s"%str(self.class_version))
+            print("Fileversion %s"%str(self.file_version))
 
         # Determine size of file
         self.datafile.seek(0,2)
         self.filesize = self.datafile.tell()
         self.num_frames = self.filesize/self.frame_size-1
         self.datafile.seek(self.frame_size,0)
-        print "Filesize", self.filesize
-        print "Number of frames", self.num_frames
+        if self.debug:
+            print "Filesize", self.filesize
+            print "Number of frames", self.num_frames
+
+    def __del__(self):
+        self.datafile.close()
 
     def unpack(self, data):
         return struct.unpack(self.data_format, data)
@@ -88,7 +99,7 @@ class AurynBinarySpikeFile:
         start_pos = idx_start*self.frame_size
         self.datafile.seek(start_pos,0)
 
-        bufsize = 2048
+        bufsize = 1024*1024
         spike_times = []
         while idx_cur < idx_stop:
             if idx_stop-idx_cur<bufsize:
@@ -108,14 +119,23 @@ class AurynBinarySpikes:
         for filename in self.filenames:
             self.spike_files.append( AurynBinarySpikeFile(filename) )
 
-
-    def get_spikes_from_interval( self, *args, **kwargs ):
+    def get_spikes_from_interval( self, t_start, t_stop ):
         spikes = []
         for spk in self.spike_files:
-            spikes.extend( spk.get_spikes_from_interval( *args, **kwargs ) )
+            spikes.extend( spk.get_spikes_from_interval( t_start, t_stop ) )
 
         spikes.sort(key=lambda tup: tup[0])
         return spikes
+
+    def compute_linear_receptive_field(self, stim_times, time_window=100e-3, max_neuron_id=1024):
+        hist = np.zeros(max_neuron_id) 
+        for t_spike in stim_times:
+            spikes = self.get_spikes_from_interval(t_spike-time_window, t_spike)
+            sar = np.array(spikes)[:,1]
+            for spk in sar:
+                if spk < max_neuron_id:
+                    hist[spk] += 1
+        return hist
 
     
 
@@ -123,9 +143,15 @@ class AurynBinarySpikes:
 # sar = np.array(spikes)
 # pl.scatter(sar[:,0],sar[:,1])
 
+t_win = 100e-3
+num_neurons = 4096 
+nid = 12
+
 filename = "/home/zenke/data/sim/rf2.0.e.spk"
 spkf     = AurynBinarySpikeFile(filename)
-t_spikes = spkf.get_spike_times_from_interval(0, 1000, 1500)
+
+print "Getting firing times"
+t_spikes = spkf.get_spike_times_from_interval(nid, 1000, 1200)
 
 filenames = ["/home/zenke/data/sim/rf2.0.s.spk",    
              "/home/zenke/data/sim/rf2.1.s.spk",
@@ -134,18 +160,8 @@ filenames = ["/home/zenke/data/sim/rf2.0.s.spk",
 spks      = AurynBinarySpikes(filenames)
 
 
-hist = np.zeros(4096)
-t_off = 100e-3
-
 print "Computing RF"
-for t_spike in t_spikes:
-    spikes = spks.get_spikes_from_interval(t_spike-t_off, t_spike)
-    sar = np.array(spikes)[:,1]
-    for spk in sar:
-        hist[spk] += 1
-    
-
-print hist
-pl.imshow(hist.reshape((64,64)))
+hist = spks.compute_linear_receptive_field(t_spikes, t_win, num_neurons)
+pl.imshow(hist.reshape((64,64)), origin='lower')
 pl.colorbar()
 pl.show()
