81bd9398553de6b96aed605cc13f607d9cbbc7a5
Friedemann Zenke <fzenke@stanford.edu>
2016-08-10 12:24:07 -0700

    Fixes typo

--- tools/python/membrane_potential.py
+++ tools/python/membrane_potential.py
@@ -12,9 +12,9 @@ t_from=0.2
 t_to  =2.5
 
 sf = AurynBinaryStateFile(filename)
-spikes = np.array(sf.get_data(t_from, t_to))
+mem = np.array(sf.get_data(t_from, t_to))
 
-pl.plot(spikes[:,0], spikes[:,1])
+pl.plot(mem[:,0], mem[:,1])
 pl.xlabel("Time [s]")
 pl.ylabel("Membrane potential [V]")
 pl.show()
