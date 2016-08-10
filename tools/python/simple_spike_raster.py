d7c0d0a7a26afe85928aba0fbda39b22aba42ec9
Friedemann Zenke <fzenke@stanford.edu>
2016-08-10 11:20:22 -0700

    Adds examples for Auryn Python toolkit

---
+++ tools/python/simple_spike_raster.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import numpy as np
+import pylab as pl
+from auryn_binary_tools import *
+
+# This code snipped assumes that you have run the example simulation
+# sim_coba_binmon with default paramters. 
+# This generates spk output files under /tmp/
+
+
+filename  =  "/tmp/coba.0.e.spk"
+seconds = 0.1
+
+sf = AurynBinarySpikeFile(filename)
+spikes = np.array(sf.get_last(seconds))
+
+pl.scatter(spikes[:,0], spikes[:,1])
+pl.xlabel("Time [s]")
+pl.ylabel("Neuron ID")
+pl.show()
+    
+
+
