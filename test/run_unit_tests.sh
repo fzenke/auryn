2c6cc1e7478d15814ce4c7d9caf807c09463492c
Friedemann Zenke <fzenke@stanford.edu>
2016-08-11 11:49:41 -0700

    Adds simple unit test script for AurynStateVector

--- test/run_unit_tests.sh
+++ test/run_unit_tests.sh
@@ -2,6 +2,9 @@
 
 BUILDDIR="../build/release"
 $BUILDDIR/test/src/test_AurynVector && \
+$BUILDDIR/test/src/test_AurynStateVector && \
 $BUILDDIR/test/src/test_EulerTrace && \
 $BUILDDIR/test/src/test_LinearTrace && \
-$BUILDDIR/test/src/test_ComplexMatrix 
+$BUILDDIR/test/src/test_ComplexMatrix && exit 0
+
+exit 1
