8bd32f0d811ca5a47acdb69742533a9fe3f27bd8
Friedemann Zenke <fzenke@stanford.edu>
2016-08-11 12:38:34 -0700

    Updates unit tests vor AurynVector and derived classes

--- test/src/test_AurynStateVector.cpp
+++ test/src/test_AurynStateVector.cpp
@@ -1,20 +1,30 @@
-#define BOOST_TEST_MODULE AurynVector test 
+#define BOOST_TEST_MODULE AurynStateVector test 
 #include <boost/test/unit_test.hpp>
 #include "../../src/AurynVector.h"
+#include "../../src/auryn_definitions.h"
+
+int expected_size( int n ) 
+{
+	const int n_simd = SIMD_NUM_OF_PARALLEL_FLOAT_OPERATIONS;
+	int n_expected = n;
+	if ( n%n_simd ) n_expected = ((n/n_simd)+1)*n_simd;
+	return n_expected;
+}
 
 BOOST_AUTO_TEST_CASE( resizing ) {
 
 	int n = 10;
-    auryn::AurynVector<int> v( n );
+
+    auryn::AurynStateVector v( n );
 	for ( int i = 0 ; i < n ; ++i ) v.set(i,i);
 
-    BOOST_CHECK_EQUAL( v.size, n );
+    BOOST_CHECK_EQUAL( v.size, expected_size(n) );
 
 	for ( int i = 0 ; i < n ; ++i ) 
 		BOOST_CHECK_EQUAL( v.data[i], i );
 
 	v.resize( 2*n );
-	BOOST_CHECK_EQUAL( v.size, 2*n );
+	BOOST_CHECK_EQUAL( v.size, expected_size(2*n) );
 
 	for ( int i = 0 ; i < n ; ++i ) 
 		BOOST_CHECK_EQUAL( v.data[i], i );
@@ -22,16 +32,16 @@ BOOST_AUTO_TEST_CASE( resizing ) {
 	for ( int i = n ; i < 2*n ; ++i ) 
 		BOOST_CHECK_EQUAL( v.data[i], 0 );
 
-	v.resize( n/2 );
-	BOOST_CHECK_EQUAL( v.size, n/2 );
+	v.resize( n/5 );
+	BOOST_CHECK_EQUAL( v.size, expected_size(n/5) );
 
-	for ( int i = 0 ; i < n/2 ; ++i ) 
+	for ( int i = 0 ; i < n/5 ; ++i ) 
 		BOOST_CHECK_EQUAL( v.data[i] , i );
 }
 
 BOOST_AUTO_TEST_CASE( pow2 ) {
 	int n = 10;
-    auryn::AurynVector<float> v( n );
+    auryn::AurynStateVector v( n );
 	for ( int i = 0 ; i < n ; ++i ) v.set(i,i);
 
 	v.pow(2);
@@ -41,8 +51,8 @@ BOOST_AUTO_TEST_CASE( pow2 ) {
 
 BOOST_AUTO_TEST_CASE( write_read ) {
 	int n = 10;
-    auryn::AurynVector<float> v1( n );
-    auryn::AurynVector<float> v2( n );
+    auryn::AurynStateVector v1( n );
+    auryn::AurynStateVector v2( n );
 
 	for ( int i = 0 ; i < n ; ++i ) v1.set(i,i);
 	for ( int i = 0 ; i < n ; ++i ) v2.set(i,i);
@@ -52,6 +62,22 @@ BOOST_AUTO_TEST_CASE( write_read ) {
 
 	for ( int i = 0 ; i < n ; ++i ) 
 		BOOST_CHECK_EQUAL( v2.get(i), i );
+}
+
+BOOST_AUTO_TEST_CASE( add ) {
+	int n = 10;
+    auryn::AurynStateVector * v1 = new auryn::AurynStateVector( n );
+    auryn::AurynStateVector * v2 = new auryn::AurynStateVector( n );
+
+	for ( int i = 0 ; i < n ; ++i ) v1->set(i,i);
+	for ( int i = 0 ; i < n ; ++i ) v2->set(i,i);
+
+	v1->add(v2);
+
+	for ( int i = 0 ; i < n ; ++i ) {
+		float f = i;
+		BOOST_CHECK_EQUAL( v1->get(i), f+f );
+	}
 }
 
 
