ab56928f71e3e339c188993efb0d919b4a9cec61
Friedemann Zenke <fzenke@gmail.com>
2018-04-16 16:57:09 +0100

    Adds first implementation of NEST inspired ParrotGroup

---
+++ src/auryn/ParrotGroup.cpp
@@ -0,0 +1,45 @@
+/* 
+* Copyright 2014-2018 Friedemann Zenke
+*
+* This file is part of Auryn, a simulation package for plastic
+* spiking neural networks.
+* 
+* Auryn is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Auryn is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Auryn.  If not, see <http://www.gnu.org/licenses/>.
+*
+* If you are using Auryn or parts of it for your work please cite:
+* Zenke, F. and Gerstner, W., 2014. Limits to high-speed simulations 
+* of spiking neural networks using general-purpose computers. 
+* Front Neuroinform 8, 76. doi: 10.3389/fninf.2014.00076
+*/
+
+#include "ParrotGroup.h"
+
+using namespace auryn;
+
+ParrotGroup::ParrotGroup( SpikingGroup * source ) : SpikingGroup( source->get_size(), RANKLOCK ) 
+{
+	auryn::sys->register_spiking_group(this);
+	src = source;
+}
+
+ParrotGroup::~ParrotGroup()
+{
+}
+
+void ParrotGroup::evolve()
+{
+	delay->push_back( src->get_spikes() );
+}
+
+
