9356ed001a3b7c07565b3f6bf8a8a9d622187c2e
Friedemann Zenke <fzenke@gmail.com>
2018-09-17 09:06:39 +0100

    Updates AurynVersion in AurynVersion object.

--- src/auryn/AurynVersion.cpp
+++ src/auryn/AurynVersion.cpp
@@ -7,8 +7,8 @@ namespace auryn {
     int AurynVersion::version = 0;
     int AurynVersion::subversion = 8;
     int AurynVersion::revision_number = 1;
-    NeuronID AurynVersion::tag_binary_spike_monitor = 287960000+100*0+10*8+1*1; //!< file signature for BinarySpikeMonitor files
-    AurynState AurynVersion::tag_binary_state_monitor = 61000+100*0+10*8+1*1; //!< file signature for BinaryStateMonitor files
+    NeuronID AurynVersion::tag_binary_spike_monitor = 287960000+100*0+10*AurynVersion::version+1*AurynVersion::subversion; //!< file signature for BinarySpikeMonitor files
+    AurynState AurynVersion::tag_binary_state_monitor = 61000+100*0+10*AurynVersion::version+1*AurynVersion::subversion; //!< file signature for BinaryStateMonitor files
     std::string AurynVersion::revision_suffix = "dev-d6c1604";
     std::string AurynVersion::git_describe = "v0.8.1-12-gd6c1604";
 
