5de0e81e9c205d0a52f4b617808c1a82da503e7b
Friedemann Zenke <fzenke@gmail.com>
2023-04-17 16:30:25 +0200

    Add untested prototype of filestim group

---
+++ src/auryn/FileStimGroup.cpp
@@ -0,0 +1,219 @@
+/* 
+* Copyright 2014-2020 Friedemann Zenke
+*
+* This file is part of Auryn, a simulation package for plastic
+* spiking neural networks.
+* 
+* Auryn is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Auryn is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Auryn.  If not, see <http://www.gnu.org/licenses/>.
+*
+* If you are using Auryn or parts of it for your work please cite:
+* Zenke, F. and Gerstner, W., 2014. Limits to high-speed simulations 
+* of spiking neural networks using general-purpose computers. 
+* Front Neuroinform 8, 76. doi: 10.3389/fninf.2014.00076
+*/
+
+#include "FileStimGroup.h"
+
+using namespace auryn;
+
+boost::mt19937 FileStimGroup::order_gen = boost::mt19937(); 
+boost::uniform_01<boost::mt19937> FileStimGroup::order_die = boost::uniform_01<boost::mt19937> (order_gen);
+
+void FileStimGroup::init()
+{
+	auryn::sys->register_spiking_group(this);
+	active = true;
+	loop_grid_size = 1;
+	reset_time = 0;
+
+	seed(42);
+}
+
+FileStimGroup::FileStimGroup( NeuronID n ) : SpikingGroup(n, RANKLOCK ) 
+{
+	playinloop = false;
+	time_delay = 0;
+	time_offset = 0;
+	init();
+}
+
+FileStimGroup::FileStimGroup(NeuronID n, std::string filename, 
+		bool loop, AurynFloat delay) 
+: SpikingGroup( n , RANKLOCK )
+{
+	init();
+	playinloop = loop;
+	if ( playinloop ) set_loop_grid(auryn_timestep);
+	time_delay = (AurynTime) (delay/auryn_timestep);
+	time_offset = 0;
+	load_spikes(filename);
+}
+
+FileStimGroup::~FileStimGroup()
+{
+}
+
+bool time_compare (SpikeEvent_type a,SpikeEvent_type b) { return (a.time<b.time); }
+
+
+void FileStimGroup::clear_input_patterns() 
+{
+	for ( unsigned int i = 0 ; i<input_patterns.size() ; ++i ) {
+		delete input_patterns.at(i);
+	}
+	input_patterns.clear();
+}
+
+void FileStimGroup::load_spikes(std::string filename)
+{
+	std::ifstream spkfile;
+	clear_input_patterns();
+
+	if ( evolve_locally() ) {
+		spkfile.open(filename.c_str(),std::ifstream::in);
+		if (!spkfile) {
+			std::cerr << "Can't open input file " << filename << std::endl;
+			std::exit(1);
+		}
+	}
+
+	// make new pattern buffer
+	current_pattern = new std::vector< SpikeEvent_type >;
+
+	char buffer[255];
+	while ( spkfile.getline(buffer, 256) ) {
+		if ( buffer[0] == '#' ) continue; // skip comments
+		if ( buffer == "" and current_pattern->size()>0 ) { // empty line triggers new pattern
+			input_patterns.push_back(current_pattern);
+			current_pattern = new std::vector< SpikeEvent_type >;
+			continue;
+		}
+
+		// assume we have a line with an event
+		SpikeEvent_type event;
+		std::stringstream line ( buffer ) ;
+		double t_tmp;
+		line >> t_tmp;
+		event.time = round(t_tmp/auryn_timestep);
+		line >> event.neuronID;
+		if ( localrank(event.neuronID) ) current_pattern->push_back(event);
+	}
+	spkfile.close();
+
+	sort_spikes(current_pattern);
+
+	std::stringstream oss;
+	oss << get_log_name() << ":: Finished loading " << input_patterns.size() 
+		<< " spike events";
+	logger->info(oss.str());
+
+	current_pattern = input_patterns.at(0);
+}
+
+void FileStimGroup::sort_spikes( std::vector< SpikeEvent_type > * pattern )
+{
+	std::sort (pattern->begin(), pattern->end(), time_compare);
+}
+
+
+AurynTime FileStimGroup::get_offset_clock() 
+{
+	return sys->get_clock() - time_offset;
+}
+
+AurynTime FileStimGroup::get_next_grid_point( AurynTime time ) 
+{
+	AurynTime result = time+time_delay;
+	if ( result%loop_grid_size ) { // align to temporal grid
+		result = (result/loop_grid_size+1)*loop_grid_size;
+	}
+	return result;
+}
+
+void FileStimGroup::evolve()
+{
+	if ( active && input_patterns.size() && current_pattern->size() ) {
+		// when reset_time is reached reset the spike_iterator to The beginning and update time offset
+		if ( sys->get_clock() == reset_time ) {
+			spike_iter = current_pattern->begin(); 
+			time_offset = sys->get_clock();
+			// std::cout << "set to" << reset_time*auryn_timestep << " " << time_offset << std::endl;
+		}
+
+		while ( spike_iter != current_pattern->end() && (*spike_iter).time <= get_offset_clock() ) {
+			spikes->push_back((*spike_iter).neuronID);
+			++spike_iter;
+			// std::cout << "spike " << sys->get_time() << std::endl;
+		}
+
+		if ( spike_iter==current_pattern->end() && reset_time < sys->get_clock() && playinloop ) { // at last spike on file set new reset time
+			reset_time = get_next_grid_point(sys->get_clock());
+
+			// TODO implement next active pattern selection
+			
+			// chooses stimulus according to schema specified in stimulusmode
+			double draw, cummulative;
+			switch ( stimulus_order ) {
+				case RANDOM:
+					draw = order_die();
+					current_pattern_index = draw*input_patterns.size();
+				break;
+				case SEQUENTIAL:
+					current_pattern_index = (current_pattern_index+1)%input_patterns.size();
+				break;
+				case SEQUENTIAL_REV:
+					if ( current_pattern_index == 0 ) 
+						current_pattern_index = input_patterns.size() - 1 ;
+					else
+						--current_pattern_index;
+				break;
+				case MANUAL:
+				default:
+				break;
+			}
+			current_pattern = input_patterns.at(current_pattern_index);
+		}
+	}
+}
+
+void FileStimGroup::seed(int rndseed)
+{
+	unsigned int rnd = rndseed + sys->get_synced_seed(); // most be same on all ranks
+	order_gen.seed(rnd); 
+
+	// this is here because the seeding above alone does not seem to do anything
+	// also need to overwrite the dist operator because it makes of copy of the
+	// generator
+	// see http://www.bnikolic.co.uk/blog/cpp-boost-uniform01.html
+	order_die = boost::uniform_01<boost::mt19937> (order_gen);
+}
+
+void FileStimGroup::set_loop_grid(AurynDouble grid_size)
+{
+	playinloop = true;
+	if ( grid_size > 0.0 ) {
+		loop_grid_size = 1.0/auryn_timestep*grid_size;
+		if ( loop_grid_size == 0 ) loop_grid_size = 1;
+	}
+}
+
+void FileStimGroup::add_spike(double spiketime, NeuronID neuron_id)
+{
+		SpikeEvent_type event;
+		event.time = spiketime/auryn_timestep;
+		event.neuronID = neuron_id;
+		current_pattern->push_back(event);
+		// sort_spikes(); // FIXME
+}
+
---
+++ src/auryn/FileStimGroup.h
@@ -0,0 +1,138 @@
+/* 
+* Copyright 2014-2020 Friedemann Zenke
+*
+* This file is part of Auryn, a simulation package for plastic
+* spiking neural networks.
+* 
+* Auryn is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Auryn is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Auryn.  If not, see <http://www.gnu.org/licenses/>.
+*
+* If you are using Auryn or parts of it for your work please cite:
+* Zenke, F. and Gerstner, W., 2014. Limits to high-speed simulations 
+* of spiking neural networks using general-purpose computers. 
+* Front Neuroinform 8, 76. doi: 10.3389/fninf.2014.00076
+*/
+
+#ifndef FILEISTIMROUP_H_
+#define FILESTIMGROUP_H_
+
+#include <fstream>
+#include <sstream>
+
+#include <math.h>
+#include <vector>
+
+#include "auryn_definitions.h"
+#include "AurynVector.h"
+#include "System.h"
+#include "SpikingGroup.h"
+
+namespace auryn {
+
+/*! \brief Reads spikes from a ras file and emits them as SpikingGroup in a simulation.
+ *
+ * FileStimGroup first reads the entire ras file into memory and emits the spikes then during the simulation
+ * without file access. It supports looping over the input spikes by setting the loop argument with the constructor 
+ * to true.
+ *
+ */
+class FileStimGroup : public SpikingGroup
+{
+private:
+	AurynTime next_event_time;
+	NeuronID next_event_spike;
+	unsigned int current_pattern_index;
+
+	/*! \brief Aligns looped file input to a grid of this size */
+	AurynTime loop_grid_size;
+
+	AurynTime time_offset;
+	AurynTime reset_time;
+
+
+	std::vector< std::vector<SpikeEvent_type> * > input_patterns;
+	std::vector< SpikeEvent_type > * current_pattern;
+	std::vector< SpikeEvent_type >::const_iterator spike_iter; 
+
+	void init();
+
+	void clear_input_patterns();
+
+	AurynTime get_offset_clock();
+	AurynTime get_next_grid_point( AurynTime time );
+
+
+protected:
+	/*! Stimulus order */
+	StimulusGroupModeType stimulus_order ;
+
+	/*! \brief Seeds the random number generator for all stimulus groups of the simulation. */
+	void seed( int rndseed );
+
+	/*! generates info for what stimulus is active. Is supposed to give the same result on all nodes (hence same seed required) */
+	static boost::mt19937 order_gen; 
+	static boost::uniform_01<boost::mt19937> order_die; 
+
+
+public:
+
+	/*! \brief Switch that activates the loop mode of FileStimGroup when set to true */
+	bool playinloop;
+
+	/*! \brief Time delay after each replay of the input pattern in loop mode (default 0s) */
+	AurynTime time_delay;
+
+	/*! \brief Default constructor */
+	FileStimGroup( NeuronID n, string filename , bool loop=false, AurynFloat delay=0.0 );
+
+	/*! \brief Constructor which does not load spikes.
+	 *
+	 * Use this constructor if you want to init the group without a filename containing spikes.
+	 * Spikes can be added after initialization by calling load_spikes(filename) or by adding
+	 * spikes one by one using add_spike(spiketime, neuron_id). */
+	FileStimGroup( NeuronID n );
+
+	virtual ~FileStimGroup();
+	virtual void evolve();
+
+	/*!\brief Aligned loop blocks to a temporal grid of this size 
+	 *
+	 * The grid is applied after the delay. It's mostly there to facilitate the
+	 * synchronization of certain events or readouts.  If no grid is defined
+	 * the last input spike time will define the end of the loop window (which
+	 * often could be a fractional time). 
+	 * */
+	void set_loop_grid(AurynDouble grid_size);
+
+	/*!\brief Load spikes from file
+	 *
+	 * */
+	void load_spikes(std::string filename);
+
+	/*!\brief Adds a spike to the buffer manually and then temporally sorts the buffer
+	 *
+	 * \param spiketime the spike time 
+	 * \param neuron_id the neuron you want to spike with neuron_id < size of the group
+	 * */
+	void add_spike(double spiketime, NeuronID neuron_id=0);
+
+	/*!\brief Sorts spikes by time
+	 *
+	 * Spikes need to be sorted befure a run otherwise the behavior of this SpikingGroup is undefined
+	 * */
+	void sort_spikes( std::vector< SpikeEvent_type > * pattern );
+};
+
+}
+
+#endif /*FILESTIMROUP_H_*/
