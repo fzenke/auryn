82a7e93416aedb5a8babbfbd2acfbbe4d5413530
Friedemann Zenke <fzenke@gmail.com>
2025-03-11 09:25:07 +0100

    Bump version number

--- src/auryn/AurynVersion.cpp
+++ src/auryn/AurynVersion.cpp
@@ -6,11 +6,11 @@ namespace auryn {
 
     int AurynVersion::version = 0;
     int AurynVersion::subversion = 8;
-    int AurynVersion::revision_number = 3;
-    NeuronID AurynVersion::tag_binary_spike_monitor = 287960000+100*0+10*8+1*3; //!< file signature for BinarySpikeMonitor files
-    AurynState AurynVersion::tag_binary_state_monitor = 61000+100*0+10*8+1*3; //!< file signature for BinaryStateMonitor files
-    std::string AurynVersion::revision_suffix = "-19a9174";
-    std::string AurynVersion::git_describe = "v0.8.3rc";
+    int AurynVersion::revision_number = 4;
+    NeuronID AurynVersion::tag_binary_spike_monitor = 287960000+100*0+10*8+1*4; //!< file signature for BinarySpikeMonitor files
+    AurynState AurynVersion::tag_binary_state_monitor = 61000+100*0+10*8+1*4; //!< file signature for BinaryStateMonitor files
+    std::string AurynVersion::revision_suffix = "-30beee9";
+    std::string AurynVersion::git_describe = "v0.8.4";
 
 }
 
